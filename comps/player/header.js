65b81d3dfd9c6455cac1516926788bc0cd9d0d6d
A V. Resendiz <52089643+pytyN01@users.noreply.github.com>
2025-02-07 15:23:49 -0800

    Update header.js

--- comps/player/header.js
+++ comps/player/header.js
@@ -1,11 +1,11 @@
-import { Text, Center } from "@mantine/core";
+import { Title, Center } from "@mantine/core";
 
 export default function Header({ name }) {
   return (
     <Center mt={20}>
-      <Text weight={700} size="xl">
+      <Title weight={700} size="xl">
         {name}
-      </Text>
+      </Title>
     </Center>
   );
 }
