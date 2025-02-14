fc3dc08e1e98bd7372bfe7741b85510cb1e73962
A V. Resendiz <52089643+pytyN01@users.noreply.github.com>
2025-02-14 10:55:54 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
           />
         )}
         {audio && (
-          <Center mt={2}>
+          <Center>
             <Button
               onClick={() => setAudio(null)}
               variant="filled"
